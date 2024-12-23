20057230fe6039339803c7b9dbbf0f782642ca21
Rohit <Rohit>
2024-12-23 21:15:48 +0530

    eslint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+
   /* config options here */
   async rewrites() {
     return [
